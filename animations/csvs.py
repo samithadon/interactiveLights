c4b45f596b2547c4d9034c7ecea200a1cacbdd4b
Noura <culurienneldoreth@gmail.com>
2017-06-29 13:24:54 +0800

    added animations

---
+++ animations/csvs.py
@@ -0,0 +1,57 @@
+import sys, os
+import csv
+from PIL import Image
+
+def make_csvs(dirname, debug):
+    # make sure the dirname ends with '/' to make it easier to combine with filename
+    # TODO would be cleaner to use os.path.join for combining dir + filename
+    if dirname[-1] != '/':
+        dirname += '/'
+
+    # list of all png image files in the directory
+    pngs = [dirname+f for f in os.listdir(dirname) if f.endswith('.png') and 'debug' not in f]
+
+    for filename in pngs:
+        im = Image.open(filename)
+        print 'processing image', filename, 'size', im.size, 'mode', im.mode
+        w, h = im.size
+        # TODO check im.mode before unpacking to support RGB and RGBA, right now this assumes RGBA mode with the 4 parts r, g, b, a
+
+        coords = []
+
+        if debug:
+            drawpix = im.load()
+
+        with open(dirname + 'grid.csv', 'rb') as gridlines:
+            for line in csv.reader(gridlines):
+                x,y = int(line[0]), int(line[1])
+                r,g,b,a = im.getpixel((x,y))
+
+                if r == 255 and g == 255 and b == 255:
+                    coords.append([x,y,1])
+                elif r == 0 and g == 0 and b == 0:
+                    coords.append([x,y,0])
+                else:
+                    print 'warning: found a pixel that is neither black or white', 'r', r, 'g', g, 'b', b, 'a', a
+                    coords.append([x,y,0])
+                    
+                if debug:
+                    drawpix[x,y] = (255, 0, 0, 255)
+
+        if debug:
+            im.save(filename.strip('.png') + '_debug' + '.png')
+
+        output_csv_name = filename.strip('png') + 'csv'
+        with open(output_csv_name, 'w') as fout:
+            writer = csv.writer(fout)
+            writer.writerows(coords)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print "Please specify the directory containing the animation files"
+        quit()
+    debug = False
+    if len(sys.argv) >= 3 and sys.argv[2] == 'debug':
+        debug = True
+    make_csvs(sys.argv[1], debug)
---
+++ animations/draw_grid.py
@@ -0,0 +1,34 @@
+from PIL import Image, ImageDraw
+import math
+import csv
+
+n_rows = 30
+n_cols = 30
+dot_r = 10
+x_spacing = 24
+y_spacing = 24
+width = int(n_cols*2*dot_r + (n_cols-1)*x_spacing + math.ceil(x_spacing/2.) + 2*dot_r)
+height = n_rows*2*dot_r + (n_rows-1)*y_spacing
+
+
+img = Image.new('RGBA', (width, height))
+draw = ImageDraw.Draw(img)
+
+coords = []
+
+# background
+draw.rectangle([0,0,width,height], fill=(55,55,55))
+
+for i in range(n_rows):
+    for j in range(n_cols):
+        x = i*(x_spacing + 2*dot_r) + (j%2)*(x_spacing/2+dot_r)
+        y = j*(y_spacing + 2*dot_r)
+        if x < 0 or y < 0:
+            print 'x=', x, 'y=', y, 'i=', i, 'j=', j
+        coords.append([x+dot_r, y+dot_r]) # centerpoint
+        draw.ellipse((x, y, x + 2*dot_r, y + 2*dot_r), fill=(0,0,0))
+
+img.save('grid.png', 'PNG')
+
+writer = csv.writer(open('grid.csv', 'wb'))
+writer.writerows(coords)
