eb1e2d4ea4d6bba5c42413f42416c3e293c8a560
Noura <culurienneldoreth@gmail.com>
2017-07-28 11:48:18 +0800

    recording user animations to local files

--- draw/server.js
+++ draw/server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
+var writeFile = require('write');
 
 // serve up the web client page
 app.use(express.static(__dirname + '/client'));
@@ -12,9 +13,20 @@ app.get('/', function(req, res, next) {
 // socketio for the web client
 var fromDraw = io.of('/draw');
 fromDraw.on('connection', function(client) {
+
+    // when the client sends an animation
     client.on('csvAnm', function(d) {
         console.log('got csvAnm event');
+
+        // send it to Processing to display
         toServers.emit('bdmsg', d);
+
+        // record it locally
+        var record_name = get_user_record_name();
+        writeFile('user_data/animations/'+record_name+'.csv', d.csv)
+           .then(function() {
+              console.log('wrote animation to', record_name);
+           }); 
     });
 });
 
@@ -25,3 +37,8 @@ toServers.on('connection', function(client) {
 });
 
 server.listen(3000);
+
+function get_user_record_name() {
+    var d = new Date();
+    return d.toString() + ' ' + d.getMilliseconds() + 'ms';
+}
